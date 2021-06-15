dd7295d3ae8beb5600540db3ebe7199ef724c806
Celia Pennington <celiajpennington@gmail.com>
2021-06-15 15:47:31 -0500

    Initial Prework File

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").style.height = "250px";
+});
+document.getElementById("button2").addEventListener("click", function(){
+        document.getElementById("box").style.backgroundColor = "blue";
+    });
+    document.getElementById("button3").addEventListener("click", function(){
+        document.getElementById("box").style.opacity = .5;
+    });
+        
